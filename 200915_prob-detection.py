0d7633e3d545523424d227ab56f7362cf304e548
SY_laptop <cloudfishh@gmail.com>
2020-09-21 12:59:53 +0900

    200920 accumulation detection with prob forecast - basic

--- 200915_prob-detection.py
+++ 200915_prob-detection.py
@@ -4,6 +4,8 @@ Accumulation detection with probabilistic forecast
 2020. 09. 15. Tue.
 Soyeong Park
 """
+from funcs import *
+
 
 '''
 1. household 1개 데이터 불러오기
@@ -16,3 +18,32 @@ Soyeong Park
 4. imputation
 5. result
 '''
+
+
+##############################
+# 0. parameter setting
+# test_house_list = ['68181c16', '1dcb5feb', '2ac64232', '3b218da6', '6a638b96']
+test_house = '68181c16'
+f_fwd, f_bwd = 24, 24
+nan_len = 3
+sigma = 4
+# imputation_acc = True
+
+
+##############################
+# 1. 데이터 불러오기
+data_raw = load_labeled()
+data, nan_data = clear_head(data_raw)
+data_col = data[test_house]
+calendar = load_calendar(2017, 2019)
+df = pd.DataFrame([], index=data_col.index)
+df['values'] = data_col.copy()
+
+
+##############################
+# 2. injection
+df['injected'], df['mask_inj'] = inject_nan_acc3(data_col, p_nan=1, p_acc=0.25)
+
+
+##############################
+# 3. accumulation detection
