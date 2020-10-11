819eb8202d1af45a429ecc4df97b3ce90730f5f8
SY_Desktop <cloudfishh@gmail.com>
2020-10-11 21:08:16 +0900

    201010 DeepAR imputation - input feature holiday

--- 200915_prob-detection.py
+++ 200915_prob-detection.py
@@ -62,64 +62,64 @@ df['injected'], df['mask_inj'] = inject_nan_acc3(data_col, p_nan=1, p_acc=0.25)
 # 3-1. candidates probabilistic forecast
 # input으로 test point 이전 23 points, output으로 test point 1 point
 # training set으로는? 이전 한 달...? 너무 긴가 이전 2주로 일단.
-#
-# idx_list = np.where((df['mask_inj'] == 3) | (df['mask_inj'] == 4))[0]
-# sample_list = list()
-#
-# len_input = 23+24*6
-# len_train = 24*7*2
-#
-# total_time = time.time()
-# for i in range(len(idx_list)):
-#     idx_target = idx_list[i]
-#     if idx_target > len_input + len_train + 1:
-#         idx_trn, idx_tst = idx_target-len_input-len_train, idx_target-len_input
-#         time_trn, time_tst = pd.Timestamp(df.index[idx_trn], freq='1H'), pd.Timestamp(df.index[idx_tst], freq='1H')
-#         trn = ListDataset([{'start': time_trn, 'target': df['injected'][idx_trn:idx_trn+len_train]}], freq='1H')
-#         tst = ListDataset([{'start': time_tst, 'target': df['injected'][idx_tst:idx_tst+len_input+1]}], freq='1H')
-#     else:
-#         # 데이터는 리버스로 넣고, timestamp는 정상적으로 넣고.
-#         idx_trn, idx_tst = idx_target+len_input+len_train, idx_target+len_input
-#         time_trn = pd.Timestamp(df.index[idx_target], freq='1H') - pd.Timedelta(value=len_input+len_train, unit='hours')
-#         time_tst = pd.Timestamp(df.index[idx_target], freq='1H') - pd.Timedelta(value=len_input, unit='hours')
-#         # trn = ListDataset([{'start': time_trn, 'target': df['injected'][idx_trn:idx_trn+len_train]}], freq='1H')
-#         # tst = ListDataset([{'start': time_tst, 'target': df['injected'][idx_tst:idx_tst+len_input+1]}], freq='1H')
-#         trn = ListDataset([{'start': time_trn,
-#                             'target': df['injected'][idx_target+len_input+1:idx_target+len_input+len_train+1][::-1]}], freq='1H')
-#         tst = ListDataset([{'start': time_tst,
-#                             'target': df['injected'][idx_target:idx_target+len_input+1][::-1]}], freq='1H')
-#
-#     estimator = DeepAREstimator(
-#         freq='1H',
-#         prediction_length=1,
-#         context_length=len_input,
-#         num_layers=2,
-#         num_cells=40,
-#         cell_type='lstm',
-#         dropout_rate=0.1,
-#         # use_feat_dynamic_real=True,
-#         # embedding_dimension=20,
-#         scaling=True,
-#         lags_seq=None,
-#         time_features=None,
-#         trainer=Trainer(ctx=mx.cpu(0), epochs=10, learning_rate=1E-3, hybridize=True, num_batches_per_epoch=200, ),
-#     )
-#     print(f'*** {i}th candidate training start ***')
-#     start_time = time.time()
-#     predictor = estimator.train(trn)
-#     print(f'*** {i}th candidate training end ***')
-#     print(f'*** elapsed time {time.time() - start_time} secs ***')
-#     forecast_it, ts_it = make_evaluation_predictions(
-#         dataset=tst,  # test dataset
-#         predictor=predictor,  # predictor
-#         num_samples=1000,  # number of sample paths we want for evaluation
-#     )
-#     forecasts = list(forecast_it)
-#     tss = list(ts_it)
-#     sample_list.append(forecasts[0].samples.reshape(1000,))
-#
-# print(f'***** Total elapsed time {time.time()-total_time} secs')
-# pd.DataFrame(sample_list).to_csv('result_deepar.csv')
+
+idx_list = np.where((df['mask_inj'] == 3) | (df['mask_inj'] == 4))[0]
+sample_list = list()
+
+len_input = 23
+len_train = 24*7*4
+
+total_time = time.time()
+for i in range(len(idx_list)):
+    idx_target = idx_list[i]
+    if idx_target > len_input + len_train + 1:
+        idx_trn, idx_tst = idx_target-len_input-len_train, idx_target-len_input
+        time_trn, time_tst = pd.Timestamp(df.index[idx_trn], freq='1H'), pd.Timestamp(df.index[idx_tst], freq='1H')
+        trn = ListDataset([{'start': time_trn, 'target': df['injected'][idx_trn:idx_trn+len_train]}], freq='1H')
+        tst = ListDataset([{'start': time_tst, 'target': df['injected'][idx_tst:idx_tst+len_input+1]}], freq='1H')
+    else:
+        # 데이터는 리버스로 넣고, timestamp는 정상적으로 넣고.
+        idx_trn, idx_tst = idx_target+len_input+len_train, idx_target+len_input
+        time_trn = pd.Timestamp(df.index[idx_target], freq='1H') - pd.Timedelta(value=len_input+len_train, unit='hours')
+        time_tst = pd.Timestamp(df.index[idx_target], freq='1H') - pd.Timedelta(value=len_input, unit='hours')
+        # trn = ListDataset([{'start': time_trn, 'target': df['injected'][idx_trn:idx_trn+len_train]}], freq='1H')
+        # tst = ListDataset([{'start': time_tst, 'target': df['injected'][idx_tst:idx_tst+len_input+1]}], freq='1H')
+        trn = ListDataset([{'start': time_trn,
+                            'target': df['injected'][idx_target+len_input+1:idx_target+len_input+len_train+1][::-1]}], freq='1H')
+        tst = ListDataset([{'start': time_tst,
+                            'target': df['injected'][idx_target:idx_target+len_input+1][::-1]}], freq='1H')
+
+    estimator = DeepAREstimator(
+        freq='1H',
+        prediction_length=1,
+        context_length=len_input,
+        num_layers=2,
+        num_cells=40,
+        cell_type='lstm',
+        dropout_rate=0.1,
+        # use_feat_dynamic_real=True,
+        # embedding_dimension=20,
+        scaling=True,
+        lags_seq=None,
+        time_features=None,
+        trainer=Trainer(ctx=mx.cpu(0), epochs=10, learning_rate=1E-3, hybridize=True, num_batches_per_epoch=200, ),
+    )
+    print(f'*** {i}th candidate training start ***')
+    start_time = time.time()
+    predictor = estimator.train(trn)
+    print(f'*** {i}th candidate training end ***')
+    print(f'*** elapsed time {time.time() - start_time} secs ***')
+    forecast_it, ts_it = make_evaluation_predictions(
+        dataset=tst,  # test dataset
+        predictor=predictor,  # predictor
+        num_samples=1000,  # number of sample paths we want for evaluation
+    )
+    forecasts = list(forecast_it)
+    tss = list(ts_it)
+    sample_list.append(forecasts[0].samples.reshape(1000,))
+
+print(f'***** Total elapsed time {time.time()-total_time} secs')
+pd.DataFrame(sample_list).to_csv('result_deepar.csv')
 
 
 # 3-2. z-score
---
+++ 201010_deepar-imp_separate.py
@@ -0,0 +1,148 @@
+"""
+Accumulation detection with nearest neighbor,
+imputation with DeepAR
++) separate sequences by W/N days
+
+2020. 10. 10. Sat.
+Soyeong Park
+"""
+##############################
+from funcs import *
+import time
+import mxnet as mx
+from gluonts.dataset.common import ListDataset
+from gluonts.model.deepar import DeepAREstimator
+from gluonts.trainer import Trainer
+from gluonts.evaluation.backtest import make_evaluation_predictions
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from matplotlib import pyplot as plt
+
+
+##############################
+# 0. parameter setting
+# test_house_list = ['68181c16', '1dcb5feb', '2ac64232', '3b218da6', '6a638b96']
+test_house = '68181c16'
+f_fwd, f_bwd = 24, 24
+nan_len = 3
+
+
+##############################
+# 1. load dataset
+data_raw = load_labeled()
+data, nan_data = clear_head(data_raw)
+data_col = data[test_house]
+calendar = load_calendar(2017, 2019)
+df = pd.DataFrame([], index=data_col.index)
+df['values'] = data_col.copy()
+df['nan'] = chk_nan_bfaf(data_col)
+df['holiday'] = calendar.loc[pd.Timestamp(df.index[0]):pd.Timestamp(df.index[-1])]
+
+
+##############################
+# 2. injection
+df['injected'], df['mask_inj'] = inject_nan_acc3(data_col, p_nan=1, p_acc=0.25)
+
+
+##############################
+# 2.5. sepearte cases
+df['holiday'] = calendar[data_col.index[0]:data_col.index[-1]]
+
+df = df[df['holiday'] == 0]        # work
+# df = df[df['holiday'] == 1]   # non-work
+
+
+##############################
+# 3. get the sample with nearest neighbor method
+idx_list = np.where((df['mask_inj'] == 3) | (df['mask_inj'] == 4))[0]
+nan_mask = df['nan'].copy()
+
+sample_list, mean_list, std_list = list(), list(), list()
+for i in range(len(idx_list)):
+    idx_target = idx_list[i]
+    sample, m, s = nearest_neighbor(data_col, df['nan'].copy(), idx_target, calendar)
+    sample_list.append(sample)
+    mean_list.append(m)
+    std_list.append(s)
+smlr_sample = pd.DataFrame(sample_list)
+# smlr_sample.to_csv('result_nearest.csv')
+
+
+# 3-2. z-score
+# smlr_sample = pd.read_csv('result_nearest.csv', index_col=0)
+cand = df[(df['mask_inj'] == 3) | (df['mask_inj'] == 4)].copy()
+z_score = (cand['injected'].values-smlr_sample.mean(axis=1))/smlr_sample.std(axis=1)
+cand['z_score'] = z_score.values
+
+# df_z = df.copy()
+df['z_score'] = np.nan
+df['z_score'][(df['mask_inj'] == 3) | (df['mask_inj'] == 4)] = z_score.values
+
+# 3-3. determine threshold for z-score
+# x-axis) z-score threshold [0, 10], y-axis) # of detected acc.
+# detection = list()
+# for z in np.arange(0, 40, 0.1):
+#     detection.append([z,
+#                       sum((cand['mask_inj'] == 4) & (cand['z_score'] > z)),
+#                       sum((cand['mask_inj'] == 3) & (cand['z_score'] > z)),     # false positive (true nor, detect acc)
+#                       sum((cand['mask_inj'] == 4) & (cand['z_score'] < z))])    # false negative (true acc, detect nor)
+# detection = pd.DataFrame(detection, columns=['z-score', 'detected_acc', 'false_positive', 'false_negative'])
+
+# threshold = 7.5
+threshold = 3.4
+idx_detected_nor = np.where(((df['mask_inj'] == 3) | (df['mask_inj'] == 4)) & (df['z_score'] < threshold))[0]
+idx_detected_acc = np.where(((df['mask_inj'] == 3) | (df['mask_inj'] == 4)) & (df['z_score'] > threshold))[0]
+detected = np.zeros(len(data_col))
+detected[np.where((df['mask_inj'] == 3) | (df['mask_inj'] == 4))] = 3
+detected[idx_detected_acc.astype('int')] = 4
+df['mask_detected'] = detected
+
+
+##############################
+# 4. imputation
+idx_cand = np.where((df['mask_inj']==3)|(df['mask_inj']==4))[0]
+
+len_unit = 24   # context_length + prediction length
+len_train = 24*7*4
+
+total_time = time.time()
+sample_fwd, sample_bwd = list(), list()
+for idx in idx_cand[:2]:
+    trn_fwd, tst_fwd, trn_bwd, tst_bwd = bidirec_dataset_deepar(df, idx, len_unit, len_train)
+
+    # forward
+    start_time = time.time()
+    print(f'*** {idx} index forward forecast start')
+    estimator = model_deepar(len_unit, df['mask_detected'][idx], epochs=1)
+    predictor = estimator.train(trn_fwd)
+    forecast_it, _ = make_evaluation_predictions(
+        dataset=tst_fwd,  # test dataset
+        predictor=predictor,  # predictor
+        num_samples=1000  # number of sample paths we want for evaluation
+    )
+    forecast_fwd = list(forecast_it)
+    sample_fwd.append(forecast_fwd[0].samples.transpose())
+    print(f'*** {idx} index forward forecast end - elapsed time {time.time()-start_time}')
+
+    # backward
+    print(f'*** {idx} index backward forecast start')
+    estimator = model_deepar(len_unit, df['mask_detected'][idx])
+    predictor = estimator.train(trn_bwd)
+    forecast_it, _ = make_evaluation_predictions(
+        dataset=tst_bwd,  # test dataset
+        predictor=predictor,  # predictor
+        num_samples=1000,  # number of sample paths we want for evaluation
+    )
+    forecast_bwd = list(forecast_it)
+    sample_bwd.append(forecast_bwd[0].samples.transpose()[::-1])    # backward는 거꾸로 다시 뒤집어줘야되죠
+    print(f'*** {idx} index backward forecast end - elapsed time {time.time()-start_time}')
+print(f'***** COMPLETED - total elasped time {time.time() - total_time}')
+
+
+sample_fwd_np, sample_bwd_np = np.array(sample_fwd[0]), np.array(sample_bwd[0])
+for s in range(len(sample_fwd)-1):
+    ss = s + 1
+    sample_fwd_np = np.concatenate((sample_fwd_np, sample_fwd[ss]))
+    sample_bwd_np = np.concatenate((sample_bwd_np, sample_bwd[ss]))
+
+pd.DataFrame(sample_fwd_np).to_csv('201011_deepar_fwd.csv')
+pd.DataFrame(sample_bwd_np).to_csv('201011_deepar_bwd.csv')
