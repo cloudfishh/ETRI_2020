9f52cbb0f42a76de08dd46a9134808b0dd8a87b8
SY_laptop <cloudfishh@gmail.com>
2020-10-13 00:06:52 +0900

    201011 DeepAR imputation - separated

--- 201011_deepar_fwdbwd.py
+++ 201011_deepar_fwdbwd.py
@@ -102,12 +102,135 @@ sample_bwd = np.append(idx_list_temp, sample_bwd.values, axis=1)
 df['imp_const'] = df['injected'].copy()
 df['imp_no-const'] = df['injected'].copy()
 
+i = 0
 for idx in idx_cand:
     if df['mask_detected'][idx] == 3:       # detected normal
-        df['imp_const'][idx+1:idx+4] =
-        df['imp_no-const'][idx+1:idx+4] =
-        pass
+        pred_len = nan_len
+        df['imp_const'][idx+1:idx+4] = (sample_fwd[i:i+pred_len, 1:].mean(axis=1) + sample_bwd[i:i+pred_len, 1:].mean(axis=1))/2
+        df['imp_no-const'][idx+1:idx+4] = (sample_fwd[i:i+pred_len, 1:].mean(axis=1) + sample_bwd[i:i+pred_len, 1:].mean(axis=1))/2
+        i += pred_len
     else:           # detected acc.
         # 4-1. w/ const.
+        pred_len = nan_len + 1
+        r = df['injected'][idx] / sum((sample_fwd[i:i+pred_len,1:].mean(axis=1) + sample_bwd[i:i+pred_len,1:].mean(axis=1)) / 2)
+        df['imp_const'][idx:idx+4] = r*((sample_fwd[i:i+pred_len,1:].mean(axis=1) + sample_bwd[i:i+pred_len,1:].mean(axis=1)) / 2)
         # 4-2. w/o const.
-        pass
+        pred_len = nan_len
+        df['imp_no-const'][idx+1:idx+4] = (sample_fwd[i:i+pred_len,1:].mean(axis=1) + sample_bwd[i:i+pred_len,1:].mean(axis=1)) / 2
+        i += (nan_len+1)
+
+
+
+##############################
+# 5-0. result - prob forecast accuracy
+# y_pred_prob = prob_sample.mean(axis=1)
+# y_true = cand['values']
+# print('Probabilistic forecast mean accuracy')
+# print(f'RMSE: {mean_squared_error(y_true, y_pred_prob)**(1/2)}')
+# print(f' MSE: {mean_squared_error(y_true, y_pred_prob)}')
+# print(f' MAE: {mean_absolute_error(y_true, y_pred_prob)}')
+# print(f'MAPE: {mape(y_true, y_pred_prob)}\n')
+#
+# y_pred_smlr = smlr_sample.mean(axis=1)
+# print('Nearest neighbor accuracy')
+# print(f'RMSE: {mean_squared_error(y_true, y_pred_smlr)**(1/2)}')
+# print(f' MSE: {mean_squared_error(y_true, y_pred_smlr)}')
+# print(f' MAE: {mean_absolute_error(y_true, y_pred_smlr)}')
+# print(f'MAPE: {mape(y_true, y_pred_prob)}\n')
+
+
+##############################
+# 5-1. result - detection confusion matrix
+# idx_injected = np.where((df['mask_inj'] == 3) | (df['mask_inj'] == 4))[0]
+# idx_real_nor = np.where(df['mask_inj'] == 3)[0]
+# idx_real_acc = np.where(df['mask_inj'] == 4)[0]
+#
+# idx_detected = np.isin(idx_injected, idx_detected_acc)
+# idx_real = np.isin(idx_injected, idx_real_acc)
+# cm = confusion_matrix(idx_real, idx_detected)
+#
+# plt.rcParams.update({'font.size': 14})
+# plt.figure()
+# sns.heatmap(cm, annot=True, fmt='d', annot_kws={'size': 20}, square=True, cmap='Greys',     # 'gist_gray': reverse
+#             xticklabels=['normal', 'accumulation'], yticklabels=['normal', 'accumulation'])
+# # plt.title(f'{test_house}, nan_length=3, {sigma}'+r'$\sigma$', fontsize=14)
+# plt.title(f'{test_house}, nan_length=3, threshold={threshold}', fontsize=14)
+# plt.xlabel('Predicted label')
+# plt.ylabel('True label')
+
+
+##############################
+# 5-2. result -  imputation accuracy
+# (1) true normal / predicted normal
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==3)&(df['mask_detected']==3))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('1. true normal / predicted normal')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
+
+# (2) true normal / predicted acc - 2 cases: w or w/o const.
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==3)&(df['mask_detected']==4))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('2-1. true normal / predicted acc - with const.')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==3)&(df['mask_detected']==4))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_no-const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('2-2. true normal / predicted acc - w/o const.')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
+
+# (3) true acc / predicted normal
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==4)&(df['mask_detected']==3))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('3. true acc / predicted normal')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
+
+# (4) true acc / predicted acc - 2 cases: w or w/o const.
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==4)&(df['mask_detected']==4))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('4-1. true acc / predicted acc - with const.')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
+result_true, result_impt = [], []
+for idx in np.where((df['mask_inj']==4)&(df['mask_detected']==4))[0]:
+    result_true.append(df['values'][idx:idx+4])
+    result_impt.append(df['imp_no-const'][idx:idx+4])
+result_true = np.array(result_true)
+result_impt = np.array(result_impt)
+print('4-2. true acc / predicted acc - w/o const.')
+print(f'   MAE {mean_absolute_error(result_true, result_impt)}')
+print(f'  RMSE {mean_squared_error(result_true, result_impt)**(1/2)}')
+print(f'  MAPE {mape(result_true, result_impt)}\n')
+
